766701444096131807e55610e5417b015f8f49b2
Venkatesh-997 <133475266+Venkatesh-997@users.noreply.github.com>
2024-02-08 20:13:00 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull venkateshkaranam/codebuild-repo:1.0
+docker pull venkateshkaranam/codebuild-repo
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 venkateshkaranam/codebuild-repo:1.0
