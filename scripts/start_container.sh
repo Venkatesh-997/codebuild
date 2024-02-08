e64dd0628feaa2850c7df7b3611fa3ca72e6c100
Venkatesh-997 <133475266+Venkatesh-997@users.noreply.github.com>
2024-02-08 20:36:00 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull venkateshkaranam/codebuild-repo
+docker pull venkateshkaranam/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 venkateshkaranam/codebuild-repo
+docker run -d -p 5000:5000 venkateshkaranam/simple-python-flask-app
